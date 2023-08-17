7a6cdad6df030d651f337e95e14796f98928c4a1
xuanlv <xuanlv@greatonce.com>
2023-08-17 11:17:51 +0800

    chore: init project

---
+++ src/main/java/com/cx/frameworklearning/FrameworkLearningApplication.java
@@ -0,0 +1,13 @@
+package com.cx.frameworklearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkLearningApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FrameworkLearningApplication.class, args);
+  }
+
+}
